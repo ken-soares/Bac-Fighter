287c8feb43f9ea68eab403ba86680809eec46fe9
meowlusine <96616995+meowlusine@users.noreply.github.com>
2023-05-11 12:24:15 +0200

    Update game.py

--- src/game.py
+++ src/game.py
@@ -1,3 +1,10 @@
 from player import Player
 from interactions import *
 
+def Game():
+  Eleve=Player(20, "../res/Eleve_Pose1.png",(1080,580))
+  Prof=Player(2000, "../res/Prof_Perso1_position1.png",(100,580))
+  while True:
+    Eleve.move()
+    Prof.move()
+    collision(Eleve,Prof)
