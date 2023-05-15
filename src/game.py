3ad50b0753f6ecaf116ac47f467234cf16112379
Kenneth Soares <96950526+ken-soares@users.noreply.github.com>
2023-05-15 17:21:31 +0200

    Update game.py

--- src/game.py
+++ src/game.py
@@ -2,13 +2,14 @@ from .player import Player
 from .interactions import *
 
 def Game():
-  Eleve=Player(20, "../res/Eleve_Pose1.png",(1080,580))
-  Prof=Player(2000, "../res/Prof_Perso1_position1.png",(100,580))
+  Eleve=Player(20, "res/Eleve_Pose1.png",(1080,580))
+  Prof=Player(2000, "res/Prof_Perso1_position1.png",(100,580))
+
   while True:
     for event in pygame.event.get():
       if event.type == pygame.KEYDOWN:
-         Eleve.move()
-         Prof.move()
-         attack(Eleve,Prof)
-   
+         Eleve.move(event)
+         Prof.move(event)
+         attack(Eleve,Prof, event)
+     
     
