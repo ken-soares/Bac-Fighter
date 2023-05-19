04f71ef4e06860374f4059fdee9e6a125b3cde94
OceT9 <131007832+OceT9@users.noreply.github.com>
2023-05-19 16:52:45 +0200

    Update player.py

--- src/player.py
+++ src/player.py
@@ -9,10 +9,12 @@ pygame.init()
 
 class Player(pygame.sprite.Sprite):
 
-  def __init__(self,pvmax,image, position): # position tuple (x,y)
+  def __init__(self,pvmax,image, position,name): # position tuple (x,y)
     super().__init__()
+    self.name=name
     self.pvs = (pvmax,pvmax) # (pv , pvmax)
-    self.img = pygame.image.load(image).convert()
+    self.img = pygame.image.load(image).convert_alpha()
+    self.img = pygame.transform.scale(self.img,(200,200))
     self.rect = self.img.get_rect() # les dimensions de l'image
     self.rect.x = position[0]
     self.rect.y = position[1]
@@ -20,16 +22,17 @@ class Player(pygame.sprite.Sprite):
 
     
 
-  def move(self,event):
-        if self.pvs[1]==2000:
-          if event.key==K_z:
-            self.rect.y-=25
-          elif event.key==K_d:
-            if not collision(Prof,Eleve):
-              self.rect.x+=10
-          elif event.key==K_q:
-            self.rect.x-=10
-        else:
+  def move(self,event,Prof,Eleve):
+    if self.pvs[1]==2000:
+      if event.key==K_z:
+        self.rect.y-=25
+      elif event.key==K_d:
+        if not collision(Prof,Eleve):
+          self.rect.x+=10
+      elif event.key==K_q:
+        self.rect.x-=10
+      else:
+        if self.name=="Eleve":
           if event.key==K_UP:
             self.rect.y-=25
           elif event.key==K_RIGHT:
@@ -39,4 +42,4 @@ class Player(pygame.sprite.Sprite):
               self.rect.x-=10
 
   def draw_player(self,scrn):
-    scrn.blit(self.image,(self.rect.x,self.rect.y)) 
+    scrn.blit(self.img,(self.rect.x,self.rect.y)) 
