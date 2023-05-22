066be7995013be5f1d79c115a1134e758ba3ee28
Kenneth Soares <96950526+ken-soares@users.noreply.github.com>
2023-05-22 13:40:50 +0200

    easy setup on windows

---
+++ setup.ps1
@@ -0,0 +1,6 @@
+cd "C:\Program Files\Python310\"
+.\python.exe -m pip install --upgrade pip
+.\python.exe -m pip install pygame
+clear
+echo "Installation terminee"
+Read-Host -Prompt "Appuyer sur n'importe quelle touche pour quitter"
